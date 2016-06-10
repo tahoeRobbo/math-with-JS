f4ddfc31796b231cc07d6d9084c9ba44662a0e5b
Robert Hallowell <robert.hallowell@clearcapital.com>
2016-06-10 14:06:55 -0700

    cleaned up a little, check for isNaN

--- factorial/factorial.js
+++ factorial/factorial.js
@@ -1,11 +1,16 @@
 function calculate() {
     var f = 1;
-    var x = $("#input1").val();
+    var x = parseInt($("#input1").val());
 
+    if (x <= 0 || isNaN(x)) {
+        $("#details").html("You must input a positive number");
+    } else {
+    x = parseInt(x);
     while(x > 1) {
         f *= x;
         x -= 1;
     }
 
     $("#details").html("The factorial of " + $("#input1").val() + " is: " + f.toString());
+    }
 }
